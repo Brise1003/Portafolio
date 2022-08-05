c2ceff550b95fc35c63f539dd905f76d5b8521cb
Brise103 <samuelbrise@hotmail.com>
2022-08-05 08:05:57 -0500

    Este es el Portafolio con las nuevas practicas de HTML y js

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+nombre=prompt("Hola, estas entrando en el portafolio de Francisco Briseño. ¿Cuál es tu nombre?");
+
+num=prompt("Si gusta que me comunique con usted, introduzca su número de celular en el siguiente recuadro por favor:");
+console.log("Make a call to " + nombre + " at "+ num);
